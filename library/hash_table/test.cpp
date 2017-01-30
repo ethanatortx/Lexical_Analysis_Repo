c1cf46b534e8b05a1342c06b7f1cb1873512833e
DESKTOP-DAL922I\Ethan Rivers <ethan@riversfamily.us>
2017-01-30 12:04:34 -0600

    hashing functions working correctly; base structuring of the hash namespace complete

---
+++ library/hash_table/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "hash.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[])
+{
+	string input256 = "grape";
+	string output256 = hash::algorithm::hash<hash::algorithm::SHA256>(input256);
+
+	cout << "sha256('"<< input256 << "'):" << output256 << endl;
+
+	string input512 = "grape";
+	string output512 = hash::algorithm::hash<hash::algorithm::SHA512>(input512);
+ 
+	cout << "sha512('"<< input512 << "'):" << output512 << endl;
+	return 0;
+}
