e0433578edce45a222df367ca9f5db11724f5b82
DESKTOP-DAL922I\Ethan Rivers <ethan@riversfamily.us>
2017-03-06 11:19:51 -0600

    btrees hell yeah

--- library/btree/btree.h
+++ library/btree/btree.h
@@ -1,421 +1,266 @@
 #ifndef BTREE_H
 #define BTREE_H
 
-#include <iostream>
+#include <string>
 #include <sstream>
-#include <utility>
-#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include "error\error.h"
 
 template<class T>
-class btree {
-
+class btree
+{
 public:
-	class const_iterator;
-	class iterator;
-
-private:
 	typedef T value_type;
 	typedef T* pointer;
 	typedef T& reference;
-	typedef const T& const_reference;
+	typedef T const& const_reference;
 	typedef std::size_t size_type;
 
-	class Node {
-	public:
-		Node( T _data,
-			  Node* _left = nullptr,
-			  Node* _right = nullptr ):
-		data(_data), left(_left), right(_right) {}
-		
-		template<class E>
-		friend std::ostream& operator<<(std::ostream&, typename btree<E>::Node*);
-		template<class E>
-		friend std::istream& operator>>(std::istream&, typename btree<E>::Node*);
+	struct node;
 
+	typedef node* iterator;
+	typedef node const* const_iterator;
+
+	struct node
+	{
+		node(const T& x, node* _left = nullptr, node* _right = nullptr):
+			data(x), left(_left), right(_right) {}
+		
 		T data;
-		Node* prev;
-		Node* left;
-		Node* right;
+		node* left;
+		node* right;
 	};
 
-	// construct from initializer list
-	btree(std::initializer_list<T>);
-
-	// return count of nodes
-	size_type count_nodes(Node*);
-
-	// get string stream of data
-	template<class E>
-	friend std::stringstream to_string(typename btree<E>::Node*);
-
-	// insert element at position
-	void insert(iterator&, const T& data);
-	void insert(const_reference, Node*);
-	void insert(value_type&&, Node* = root);
-
-	// erase element
-	void erase(iterator&);
-
-public:
 	btree();
 	btree(const btree<T>&);
 
-	// return true if contains no elements
-	bool empty() const;
-
-	// clear data from the tree
+	void insert(T data);
+	iterator search(T data);
+	bool remove(T data);
 	void clear();
+	std::stringstream& to_string(std::stringstream& ss, iterator pos);
+	void deltree(iterator pos);
+	size_type size(btree<T>::iterator pos);
 
-	template<class E>
-	friend bool operator==(const btree<E>&, const btree<E>&);
-	template<class E>
-	friend bool operator!=(const btree<E>&, const btree<E>&);
 	template<class E>
 	friend std::ostream& operator<<(std::ostream&, const btree<E>&);
 	template<class E>
-	friend std::istream& operator>>(std::istream&, const btree<E>&);
-
-	// insert data into the tree
-	void insert(const_reference);
-	void insert(value_type&&);
-	void insert(const_reference, size_type);
-	void insert(const std::vector<const_reference>&);
-
-	// erase first element with data
-	void erase(const T&);
-	// erase all elements with data
-	void erase_with(const T&);
-
-	// return iterator to first element with data
-	iterator find(const T&);
-	const_iterator find(const T&) const;
-
-	// return iterator to beginning
-	iterator begin();
-	const_iterator begin() const;
-	const_iterator cbegin() const;
-
-	// return iterator to end
-	iterator end();
-	const_iterator end() const;
-	const_iterator cend() const;
+	friend void quicksort(T arr[]);
 
 private:
-	Node* root;
+	bool size_is_comparable(btree<T>::iterator _root) const;
+	void rebalance(btree<T>::iterator _root);
+	void strict_rebalance(btree<T>::iterator _root);
+	std::vector<T> data_to_vector(btree<T>::iterator _root);
+
+	node* root;
 };
 
-// constructors
 template<class T>
 btree<T>::btree():
-	root(new typename btree<T>::Node(T()))
-{
+	root(new node(T()))
+{}
 
-}
 template<class T>
-btree<T>::btree(const btree<T>& other)
+bool btree<T>::size_is_comparable(btree<T>::iterator _root) const
 {
-
-}
-
-// non-member functions
-template<class T>
-inline bool operator==(const btree<T>& lhs, const btree<T>& rhs)
-{
-	std::stringstream is_lhs, is_rhs;
-	is_lhs << lhs;
-	is_rhs << rhs;
-
-	return (is_lhs.str()) == (is_rhs.str());
+	int diff = (size(_root->left) - size(root->right));
+	return std::abs(diff) <= 1;
 }
 
 template<class T>
-inline bool operator!=(const btree<T>& lhs, const btree<T>& rhs)
+void btree<T>::rebalance(btree<T>::iterator _root)
 {
-	std::stringstream is_lhs, is_rhs;
-	is_lhs << lhs;
-	is_rhs << rhs;
-
-	return (is_lhs.str()) != (is_rhs.str());
+	if(size_is_comparable(_root))
+	{
+		rebalance(_root->left);
+		rebalance(_root->right);
+	}
+	else
+	{
+		std::vector<T> vec = data_to_vector(_root);
+		deltree(_root->left);
+		deltree(_root->right);
+		quicksort(vec, 0, vec.size());
+		strict_rebalance(_root, vec);
+	}
 }
 
 template<class T>
-inline std::ostream& operator<<(std::ostream& os, const btree<T>& rhs)
+void btree<T>::strict_rebalance(btree<T>::node* _root, std::vector<T>& vec)
 {
-	os << to_string(rhs.root);
-	return os;
-}
+	if(vec.size() == 0)
+	{
+		return;
+	}
+	else if(vec.size() == 1)
+	{
+		_root->data = vec[0];
+	}
+	else if(vec.size() == 2)
+	{
+		_root->data = vec[0];
+		node *left;
+		left = new node(vec[1]);
+		_root->left = left;
+	}
+	else
+	{
+		size_type median = vec.size()/2;
 
-template<class T>
-inline std::istream& operator>>(std::istream& is, btree<T>& rhs)
-{
-
-}
-
-template<class T>
-inline std::stringstream to_string(typename btree<T>::Node* root)
-{
-	if(root == nullptr) return NULL;
-
-	std::stringstream ss;
-
-	ss << root->data;
-	ss << to_string(root->left);
-	ss << to_string(root->right);
-
-	return ss;
-}
-
-template<class T>
-void btree<T>::insert(typename btree<T>::const_reference data, typename btree<T>::iterator& pos)
-{
-
-}
-template<class T>
-void btree<T>::insert(typename btree<T>::const_reference data, typename btree<T>::Node* root)
-{
+		_root->data = vec[median];
 	
+		std::vector<T> left_vec, right_vec;
+		node *left, *right;
+
+		left = new node();
+		right = new node();
+
+		_root->left = left;
+		_root->right = right;
+
+		left_vec.insert(left_vec.begin(), vec.begin(), vec.begin()+median);
+		right_vec.insert(right_vec.begin(), vec.begin()+median+1, vec.end());
+
+		strict_rebalance(_root->left, left_vec);
+		strict_rebalance(_root->right, right_vec);
+	}
 }
+
 template<class T>
-void btree<T>::insert(typename btree<T>::value_type&& data, typename btree<T>::Node* root)
+std::vector<T> btree<T>::data_to_vector(btree<T>::iterator _root)
+{
+	std::vector<T> vec;
+
+	if(_root != nullptr)
+	{
+		std::vector<T> tmp;
+
+		vec.push_back(_root->data);
+
+		tmp = data_to_vector(_root->left);
+		vec.insert(vec.end(), tmp.begin(), tmp.end());
+
+		tmp = data_to_vector(_root->right);
+		vec.insert(vec.end(), tmp.begin(), tmp.end());
+	}
+
+	return vec;
+}
+
+template<class T>
+void btree<T>::insert(btree<T>::iterator _root, btree<T>::const_reference _data)
+{
+
+	rebalance(_root);
+
+	if(_data > _root->data)
+	{
+		if(_root->right == nullptr)
+		{
+			node* n = new node(_data);
+			_root->right = n;
+		}
+		else
+		{
+			insert(_root->right, _data);
+		}
+	}
+	else if (_data <= _root->data)
+	{
+		if(_root->left == nullptr)
+		{
+			node* n = new node(_data);
+			_root->left = n;
+		}
+		else
+		{
+			insert(_root->left, _data);
+		}
+	}
+
+	rebalance(_root);
+}
+
+template<class T>
+btree<T>::iterator btree<T>::search(btree<T>::const_reference _data)
 {
 
 }
 
-// member functions
-// return total count of nodes in graph
 template<class T>
-typename btree<T>::size_type btree<T>::count_nodes(btree<T>::Node* root) // PRIVATE FUNC
+bool btree<T>::remove(btree<T>::const_reference _data)
 {
-	if(root == nullptr) return 0;
 
-	typename btree<T>::size_type count = 1;
-	count += count_nodes(root->left);
-	count += count_nodes(root->right);
+}
+
+template<class T>
+void btree<T>::clear()
+{
+
+}
+
+template<class T>
+std::stringstream& to_string(std::stringstream& ss, iterator pos)
+{
+
+}
+
+template<class T>
+void btree<T>::deltree(iterator pos)
+{
+
+}
+
+template<class T>
+typename btree<T>::size_type btree<T>::size(btree<T>::iterator _root)
+{
+	btree<T>::size_type count = 0;
+	if(_root != nullptr)
+	{
+		++count;
+		count += size(_root->left);
+		count += size(_root->right);
+	}
 
 	return count;
 }
 
-// insert data into the graph
 template<class T>
-inline void btree<T>::insert(typename btree<T>::const_reference data)
-	{ insert(data, this->root); }
-template<class T>
-inline void btree<T>::insert(typename btree<T>::value_type&& data)
-	{ insert(data, this->root); }
-template<class T>
-void btree<T>::insert(typename btree<T>::const_reference data, typename btree<T>::size_type count)
+std::ostream& operator<<(std::ostream& os, const btree<T>& B)
 {
-
-}
-template<class T>
-void btree<T>::insert(const std::vector<typename btree<T>::const_reference>& data)
-{
-
-}
-
-// erase first element with data
-template<class T>
-void btree<T>::erase(typename btree<T>::const_reference data)
-{
-
-}
-
-// erase element
-template<class T>
-void btree<T>::erase(typename btree<T>::iterator& pos) // PRIVATE FUNC
-{
-
-}
-
-// erase all elements with data
-template<class T>
-void btree<T>::erase_with(typename btree<T>::const_reference data)
-{
-
-}
-
-// return iterator to beginning
-template<class T>
-inline typename btree<T>::iterator btree<T>::begin()
-	{ return btree<T>::iterator(this->root); }
-template<class T>
-inline typename btree<T>::const_iterator btree<T>::begin() const
-	{ return btree<T>::const_iterator(this->root); }
-template<class T>
-inline typename btree<T>::const_iterator btree<T>::cbegin() const
-	{ return btree<T>::const_iterator(this->root); }
-
-// return iterator to end
-template<class T>
-inline typename btree<T>::iterator btree<T>::end()
-{
-
-}
-template<class T>
-inline typename btree<T>::const_iterator btree<T>::end() const
-{
-
-}
-template<class T>
-inline typename btree<T>::const_iterator btree<T>::cend() const
-{
-
+	os << (B.to_string());
+	return os;
 }
 
 template<class T>
-class btree<T>::const_iterator:
-	virtual public std::iterator<std::bidirectional_iterator_tag, T>
+void quicksort(std::vector<T>& arr, int left, int right)
 {
-	typedef T value_type;
-	typedef const typename btree<T>::Node* pointer;
-	typedef T& reference;
-	typedef std::bidirectional_iterator_tag iterator_category;
-	typedef std::ptrdiff_t difference_type;
+	int i = left, j = right;
+	int tmp;
+	int pivot = arr[(left + right) / 2];
 
-	friend class btree;
-	friend class btree<T>::iterator;
+	/* partition */
+	while (i <= j) {
+		while (arr[i] < pivot)
+			i++;
+		while (arr[j] > pivot)
+			j--;
+		if (i <= j) {
+			tmp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = tmp;
+			i++;
+			j--;
+		}
+	};
 
-public:
-	const_iterator(const typename btree<T>::Node* _ref = nullptr):
-		ref(_ref) {}
-	const_iterator(const typename btree<T>::iterator& other):
-		ref(other.ref) {}
-	const_iterator(const typename btree<T>::const_iterator& other):
-		ref(other.ref) {}
-
-	template<class E>
-	friend bool operator==(const typename btree<E>::const_iterator&, const typename btree<E>::const_iterator&);
-	template<class E>
-	friend bool operator!=(const typename btree<E>::const_iterator&, const typename btree<E>::const_iterator&);
-	template<class E>
-	friend bool operator<=(const typename btree<E>::const_iterator&, const typename btree<E>::const_iterator&);
-	template<class E>
-	friend bool operator<(const typename btree<E>::const_iterator&, const typename btree<E>::const_iterator&);
-	template<class E>
-	friend bool operator>=(const typename btree<E>::const_iterator&, const typename btree<E>::const_iterator&);
-	template<class E>
-	friend bool operator>(const typename btree<E>::const_iterator&, const typename btree<E>::const_iterator&);
-
-	inline typename btree<T>::const_iterator& operator=(const typename btree<T>::iterator& other)
-	{
-		this->ref = other.ref;
-		return *this;
-	}
-	inline typename btree<T>::const_iterator& operator=(const typename btree<T>::const_iterator& other)
-	{
-		this->ref = other.ref;
-		return *this;
-	}
-
-	const_iterator& operator++()
-	{
-
-	}
-	const_iterator operator++(int)
-	{
-
-	}
-
-	const_iterator& operator--()
-	{
-		ref = ref->prev;
-		return *this;
-	}
-	const_iterator operator--(int)
-	{
-		typename btree<T>::const_iterator tmp(*this);
-		ref = ref->prev;
-		return tmp;
-	}
-
-	typename btree<T>::const_iterator::reference operator*() const
-		{ return ref->data; }
-	typename btree<T>::const_iterator::pointer operator->() const
-		{}
-
-	~const_iterator() {}
-
-private:
-	const typename btree<T>::Node* ref;
-};
-
-template<class T>
-class btree<T>::iterator:
-	virtual public std::iterator<std::bidirectional_iterator_tag, T>,
-	public btree<T>::const_iterator
-{
-	typedef T value_type;
-	typedef typename btree<T>::Node* pointer;
-	typedef T& reference;
-	typedef std::bidirectional_iterator_tag iterator_category;
-	typedef std::ptrdiff_t difference_type;
-
-	friend class btree;
-	friend class btree<T>::const_iterator;
-
-public:
-	iterator(typename btree<T>::Node* _ref):
-		ref(_ref) {}
-	iterator(const typename btree<T>::iterator& other):
-		ref(other.ref) {}
-
-	template<class E>
-	friend bool operator==(const typename btree<E>::iterator&, const typename btree<E>::iterator&);
-	template<class E>
-	friend bool operator!=(const typename btree<E>::iterator&, const typename btree<E>::iterator&);
-	template<class E>
-	friend bool operator<=(const typename btree<E>::iterator&, const typename btree<E>::iterator&);
-	template<class E>
-	friend bool operator<(const typename btree<E>::iterator&, const typename btree<E>::iterator&);
-	template<class E>
-	friend bool operator>=(const typename btree<E>::iterator&, const typename btree<E>::iterator&);
-	template<class E>
-	friend bool operator>(const typename btree<E>::iterator&, const typename btree<E>::iterator&);
-
-	inline typename btree<T>::iterator& operator=(const typename btree<T>::iterator& other)
-	{
-
-	}
-
-	inline typename btree<T>::iterator& operator++()
-	{
-
-	}
-	inline typename btree<T>::iterator operator++(int)
-	{
-
-	}
-
-	inline typename btree<T>::iterator& operator--()
-	{
-		ref = ref->prev;
-		return *this;
-	}
-	inline typename btree<T>::iterator operator--(int)
-	{
-		typename btree<T>::iterator tmp(*this);
-		ref = ref->pref;
-		return tmp;
-	}
-
-	inline typename btree<T>::iterator::reference operator*() const
-		{ return ref->data; }
-	inline typename btree<T>::iterator::pointer operator->() const
-		{ return ref->left; }
-
-private:
-	typename btree<T>::Node* ref;
-};
-
-template<class T>
-inline bool operator==(const typename btree<T>::iterator& lhs, const typename btree<T>::iterator& rhs)
-{
-
-}
-template<class T>
-inline bool operator!=(const typename btree<T>::iterator& rhs, const typename btree<T>::iterator& rhs)
-{
-
+	/* recursion */
+	if (left < j)
+		quickSort(arr, left, j);
+	if (i < right)
+		quickSort(arr, i, right);
 }
 
 #endif
