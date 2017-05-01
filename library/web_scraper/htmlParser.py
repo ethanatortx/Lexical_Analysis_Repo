f34f90941138d38caf5862f208480938656c82c6
Ethan Schaffer <etanbet@gmail.com>
2017-05-01 10:35:14 -0500

    HTML Parser
    hehe spaghetti code

--- library/web_scraper/htmlParser.py
+++ library/web_scraper/htmlParser.py
@@ -6,12 +6,14 @@ except ImportError:
      from html.parser import HTMLParser
 import urllib.request as u
 import csv
+global myDict
+myDict = { }
 def check_conditions(i):
-     banned_phrases = ['all rights reserved', 'copyright', 'writen by', 'newsletter']
+     banned_phrases = ['all rights reserved', 'copyright', 'writen by', 'newsletter', 'password']
      for phrase in banned_phrases:
           if(phrase.lower() in i.lower()):
                return False
-     banned_groups = [ ['readers', 'like', 'you'], ['read', 'our'], ['cookie', 'advertising', 'policy'] ]
+     banned_groups = [ ['create', 'new', 'password'], ['Please', 'confirm', 'the', 'information', 'below'], ['readers', 'like', 'you'], ['read', 'our'], ['cookie', 'advertising', 'policy'], ['disable', 'ad', 'blocker'], ['share', 'this', 'video'], ['watch', 'this', 'video'], ['watch', 'next'], ['sign', 'in']]
      for group in banned_groups:
           strikes = 0
           for item in group:
@@ -19,13 +21,18 @@ def check_conditions(i):
                     strikes+=1
           if strikes/len(group)>.5:
                return False
+     if (i == i.upper()):
+          return False
      return True
-def analyze(article):
+def analyze(article, searchTerm, followLinks):
      h = HTMLParser()
-     response = u.urlopen(article)
+     try:
+          response = u.urlopen(article)
+     except:
+          return
      html = response.read()
-     temp = ""
      html = html.decode()
+     temp = ""
      
      title = "No Title"
      arrOfSentences = []
@@ -49,7 +56,10 @@ def analyze(article):
                temp2 = "&"
                while html[pos] != ";":
                     pos+=1
-                    temp2+=html[pos]
+                    try:
+                         temp2+=html[pos]
+                    except:
+                         return
                temp+=h.unescape(temp2)
           elif not bracketOn:
                temp+=str(i)
@@ -63,8 +73,9 @@ def analyze(article):
                          while html[pos+1]!="\"":
                               pos+=1
                               tempStr+=html[pos]
-                         if("https://ap" in tempStr):
-                              siteList.append(tempStr)
+                         if("https://" in tempStr or "http://" in tempStr) and followLinks:
+                              if(searchTerm.lower() in tempStr.lower()):
+                                   siteList.append(tempStr)
                     if('title' in html[pos:pos+6].lower()):
                          pos+=1
                          while html[pos]!=">":
@@ -79,6 +90,8 @@ def analyze(article):
           strLast = i
      arrOfSentences.append(temp)
      title = h.unescape(title)
+     if ("twitter" in title or "google plus" in title or "facebook" in title or searchTerm.lower() not in title.lower()):
+          return None
      fixedTitle = ""
      for i in title:
           if i.lower() in 'abcdefghijklmnopqrstuvwxyz1234567890':
@@ -86,15 +99,57 @@ def analyze(article):
           else:
                fixedTitle+=" "
      text_file = open(fixedTitle+".txt", "w")
+     global myDict
+     for sentence in siteList:
+          print(sentence)
      for i in arrOfSentences:
            if check_conditions(i):
-                 text_file.write(i)
-                 text_file.write('\n\n')
+                for word in sentence:
+                    if( myDict[word] == None):
+                         myDict[word] = 0
+                    myDict[word]+1
+               text_file.write(i)
+               text_file.write('\n\n')
      text_file.close()
      print("Analyzed: " + title)
      return siteList
-
-q = 'http://bigstory.ap.org/article/3b2706cb4d3f41e39137b175ff6d0b84/10-things-know-today'
-listOfArticles = analyze(q)
+searchTerm = input("Search Term: ")
+q = 'https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#safe=strict&tbm=nws&q=senate'
+q = "https://news.google.com/news?q=" + searchTerm.replace(" ", "+") + "&output=rss"
+q.encode('utf-8').strip()
+listOfArticles = []
+h = HTMLParser()
+response = u.urlopen(q)
+html = response.read()
+html = html.decode()
+pos = 0
+totalAnalyzed = 0
+while pos < len(html) and totalAnalyzed < 10:
+     if (html[pos:pos+4] == "http"):
+          tempStr = ""
+          while(html[pos] != "&"):
+               tempStr+=html[pos]
+               pos+=1
+          #print(tempStr)
+          if "twitter" not in tempStr and "thehill" not in tempStr and "news.google" not in tempStr and "nytimes" not in tempStr and "youtube" not in tempStr:
+               #The New York Times doesn't play nice with Python's urllib
+               try:
+                    for i in analyze(tempStr, searchTerm, False):
+                         print(i)
+                         listOfArticles.append(i)
+               except:
+                    totalAnalyzed -=1
+               totalAnalyzed+=1
+     pos+=1
+     if len(listOfArticles) > 20:
+          pos = len(html)
 for article in listOfArticles:
-     analyze(article)
+     analyze(article, searchTerm, False)
+print("################")
+print("### Finished ###")
+print("################")
+print(myDict)
+for word in myDict:
+     print(word)
+     print(myDict[word])
+     print("")          
